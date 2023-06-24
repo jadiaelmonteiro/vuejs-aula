24fa88d90629b7e697db35748b965866b5f69376
jadiaelmonteiro <jadiael.monteiro@gmail.com>
2023-06-24 20:37:49 -0300

    Adicionando nova rota de projetos

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -4,7 +4,28 @@
             <img style="width: 500px; height: 200px;" src="../assets/logo.svg" />
         </h1>
 
-        <button class="button" @click="Alterartema">{{ textoBotao }}</button>
+        <div class="has-text-centered">
+            <button class="button" @click="Alterartema">{{ textoBotao }}</button>
+        </div>
+
+        <nav class="painal mt-5">
+            <ul>
+                <li>
+                    <router-link to="/" class="link">
+                        <i class="fas fa-tasks">
+                            tarefas
+                        </i>
+                    </router-link>
+                </li>
+                <li class="mt-5">
+                    <router-link to="/projetos" class="link">
+                        <i class="fas fa-project-diagram">
+                            projetos
+                        </i>
+                    </router-link>
+                </li>
+            </ul>
+        </nav>
 
     </header>
 </template>
@@ -16,13 +37,13 @@ import { defineComponent } from 'vue';
 export default defineComponent({
     name: 'BarraLateral',
     emits: ['aoTemaAlterado'],
-    data () {
+    data() {
         return {
             modoEscuroAtivo: false
         }
     },
     computed: {
-        textoBotao () {
+        textoBotao() {
             if (this.modoEscuroAtivo) {
                 return 'Desativar modo escuro'
             }
@@ -30,7 +51,7 @@ export default defineComponent({
         },
     },
     methods: {
-        Alterartema(){
+        Alterartema() {
             this.modoEscuroAtivo = !this.modoEscuroAtivo;
             this.$emit('aoTemaAlterado', this.modoEscuroAtivo);
         }
@@ -52,5 +73,21 @@ header {
         padding: 2.5rem;
         height: auto;
     }
+}
+
+.panel li {
+    margin: 8px 0;
+}
+
+.link {
+    color: #fff;
+}
+
+.link:hover {
+    color: #FAF0CA;
+}
+
+.link.router-link-active {
+    color: #FAF0CA;
 }
 </style>
